598cebe20d5e917582987fba6f7ef5f776cd0932
web <web@AMSO0055Z-17.local>
2019-11-09 17:08:22 -0500

    ViewPager - Functionality Switched Placedholder Textview to Imageview Styled Viewpager Better

--- app/src/main/java/com/example/touristapp/LocationsFragment.java
+++ app/src/main/java/com/example/touristapp/LocationsFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -20,7 +21,7 @@ public class LocationsFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     private static final String ARG_PARAM3 = "param3";
 
-    private String mParam1;
+    private int mParam1;
     private String mParam2;
     private String mParam3;
 
@@ -29,10 +30,10 @@ public class LocationsFragment extends Fragment {
         // Required empty public constructor
     }
 
-        public static LocationsFragment newInstance(String param1, String param2, String param3) {
+        public static LocationsFragment newInstance(int param1, String param2, String param3) {
             LocationsFragment fragment = new LocationsFragment();
             Bundle args = new Bundle();
-            args.putString(ARG_PARAM1, param1);
+            args.putInt(ARG_PARAM1, param1);
             args.putString(ARG_PARAM2, param2);
             args.putString(ARG_PARAM3, param3);
             fragment.setArguments(args);
@@ -43,7 +44,7 @@ public class LocationsFragment extends Fragment {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             if (getArguments() != null) {
-                mParam1 = getArguments().getString(ARG_PARAM1);
+                mParam1 = getArguments().getInt(ARG_PARAM1);
                 mParam2 = getArguments().getString(ARG_PARAM2);
                 mParam3 = getArguments().getString(ARG_PARAM3);
             }
@@ -54,9 +55,9 @@ public class LocationsFragment extends Fragment {
                 Bundle savedInstanceState) {
             // Inflate the layout for this fragment
             View view = inflater.inflate(R.layout.fragment_locations, container, false);
-            TextView imagePlaceHolder = view.findViewById(R.id.centerText);
-            if (mParam1 != null) {
-                imagePlaceHolder.setText(mParam1);
+            ImageView imagePlaceHolder = view.findViewById(R.id.locationImage);
+            if (mParam1 != 0) {
+                imagePlaceHolder.setImageResource(mParam1);
             }
 
             TextView locationName = view.findViewById(R.id.locationName);
--- app/src/main/java/com/example/touristapp/ViewPager.java
+++ app/src/main/java/com/example/touristapp/ViewPager.java
@@ -42,17 +42,17 @@ public class ViewPager extends Fragment {
         @Override
         public Fragment getItem(int position) {
             switch (position){
-                case 0: return LocationsFragment.newInstance("1", "Italy", "Description1");
-                case 1: return LocationsFragment.newInstance("2","Rome","Description2");
-                case 2: return LocationsFragment.newInstance("3","Greece", "Description3");
-                case 3: return LocationsFragment.newInstance("4","Spain","Description4");
-                case 4: return LocationsFragment.newInstance("5","Rio","Description5" );
-                case 5: return LocationsFragment.newInstance("6","Japan", "Description6");
-                case 6: return LocationsFragment.newInstance("7","China","Description7");
-                case 7: return LocationsFragment.newInstance("8","Canada","Description8");
+                case 0: return LocationsFragment.newInstance(R.drawable.city1, "Chicago", "Description1");
+                case 1: return LocationsFragment.newInstance(R.drawable.city2,"Rome","Description2");
+                case 2: return LocationsFragment.newInstance(R.drawable.city3,"Greece", "Description3");
+                case 3: return LocationsFragment.newInstance(R.drawable.city1,"Spain","Description4");
+                case 4: return LocationsFragment.newInstance(R.drawable.city2,"Rio","Description5" );
+                case 5: return LocationsFragment.newInstance(R.drawable.city3,"Japan", "Description6");
+                case 6: return LocationsFragment.newInstance(R.drawable.city2,"China","Description7");
+                case 7: return LocationsFragment.newInstance(R.drawable.city1,"Canada","Description8");
 
 
-                default: return LocationsFragment.newInstance("ERROR","ERROR", "ERROR");
+                default: return LocationsFragment.newInstance(R.drawable.city1,"ERROR", "ERROR");
             }
         }
         @Override
